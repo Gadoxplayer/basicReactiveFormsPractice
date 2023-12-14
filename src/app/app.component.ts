c4bfa2f2e661fd09f7f73d5dd557e0f963e3b0bd
Jesus Campo Alvarez <eduardocampo91@gmail.com>
2023-12-13 21:13:24 -0500

    adding a hobbie form, with a get controls method to generate inputs, also a personalize validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,38 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupform: FormGroup;
+  forbddenUserNames = ['Cris', 'Anna'];
 
   ngOnInit(): void {
       this.signupform = new FormGroup({
         'userData': new FormGroup({
-          'username': new FormControl(null, Validators.required),
+          'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           'email': new FormControl(null, [Validators.required, Validators.email])          
         }),
         'gender': new FormControl('male'),
+        'hobbies': new FormArray([])
       });
   }
 
   onSubmit() {
     console.log(this.signupform);
     
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupform.get('hobbies')).push(control);
+  }//wont work in other version of angular
+
+  getControls() {
+    return (<FormArray>this.signupform.get('hobbies')).controls;
+  }
+  //*ngFor="let hobbyControl of getControls(); let i = index"
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbbiden': true}
+    }
+    return null;
   }
 }
