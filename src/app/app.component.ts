39a25ff0c0ef57ab55451d989a5ecbb4107c1e7b
EduardoCampo <eduardocampo91@gmail.com>
2024-01-03 23:22:04 -0500

    updatingform to use custom validators and asycn validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +10,42 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupform: FormGroup;
-  forbddenUserNames = ['Cris', 'Anna'];
+  forbddenUserNames = ['Cris', 'Anna', 'cris', 'anna'];
 
   ngOnInit(): void {
       this.signupform = new FormGroup({
         'userData': new FormGroup({
           'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-          'email': new FormControl(null, [Validators.required, Validators.email])          
+          'email': new FormControl(null, [Validators.required, Validators.email], this.forbbidenEmails)          
         }),
         'gender': new FormControl('male'),
         'hobbies': new FormArray([])
       });
+      // this.signupform.valueChanges.subscribe(
+      //   (value) => console.log(value)
+      // );
+      this.signupform.statusChanges.subscribe(
+        (value) => console.log(value)
+      );
+      // this.signupform.setValue({
+      //   'userData': {
+      //     'username': 'MAx',
+      //     'email': 'email@oi.com'
+      //   },
+      //   'gender': 'male',
+      //   'hobbies': []
+      // });
+      this.signupform.patchValue({
+        'userData': {
+          'username': 'anna',
+          'email': 'emailbbb@oi.com'
+        }
+      })
   }
 
   onSubmit() {
     console.log(this.signupform);
+    this.signupform.reset();
     
   }
 
@@ -42,5 +64,18 @@ export class AppComponent implements OnInit {
       return {'nameIsForbbiden': true}
     }
     return null;
+  }
+
+  forbbidenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com') {
+          resolve({'emailIsForbbiden': true})
+        } else {
+          resolve(null)
+        }
+      }, 1500)
+    });
+    return promise;
   }
 }
